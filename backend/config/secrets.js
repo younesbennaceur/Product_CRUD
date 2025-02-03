aa3648168fdbd52c7ef9c4165bdd053330133e1d
younesbennaceur <younesbennaceur06@gmail.com>
2025-02-03 22:01:13 +0100

    Creating Product Model

--- backend/config/secrets.js
+++ backend/config/secrets.js
@@ -1,8 +1,8 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
- 
+dotenv.config();
+
 export const ENV = {
- MONGO_URI : process.env.MONGO_URI ,
- PORT : process.env.PORT
-}
+  MONGO_URI: process.env.MONGO_URI,
+  PORT: process.env.PORT,
+};
